aac7745f087674910360b6994cd27c5cb526908e
MarkMedhat <markmadhat03@gmail.com>
2026-04-21 07:34:36 +0200

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-function go(id){
-  var ids=['laws','cond','ex','rv'];
-  document.querySelectorAll('.tab').forEach(function(b,i){b.classList.toggle('on',ids[i]===id)});
-  document.querySelectorAll('.panel').forEach(function(p){p.classList.toggle('on',p.id==='p-'+id)});
-}
